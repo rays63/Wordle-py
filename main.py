7a6064a7c698e0f2a9a61dd2867f4da278764972
Raymond Maharjan <raymondmhz63@gmail.com>
2022-09-06 11:53:47 +0545

    correcting bugs

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-from cmd import PROMPT
 import random
 import rich
 from rich.prompt import Prompt
 from rich.console import Console
-from Words import Words
+from Words import words
 
 
 def get_words(word):
@@ -27,29 +26,31 @@ def score_guess(guess, answer):
     score = []
     for index, letter in enumerate(guess):
         if answer[index] == guess[index]:
-            score += correct_place(index)
-        elif letter in guess:
-            score += correct_letter(index)
+            score += correct_place(guess[index])
+        elif letter in answer:
+            score += correct_letter(letter)
         else:
-            score += incorrect_letter(index)
+            score += incorrect_letter(letter)
     return ''.join(score)
 
 
 def main():
     rich.print(correct_place("welcome") + " " + correct_letter("To") +
-               " " + incorrect_letter("Wordle.Py") + "/n")
+               " " + incorrect_letter("Wordle.Py") + " ")
     tries = 6
 
     console = Console()
-    answer = get_words(Words)
+    answer = get_words(words)
 
     while tries != 0:
         tries = tries - 1
-        user_inp = Prompt.ask("Enter your Guess: ")
-        console.print(score_guess(user_inp, answer))
+        user_inp = Prompt.ask("Enter your Guess: ").upper()
+        console.print(score_guess(user_inp, answer) + " ")
         if user_inp == answer:
+            console.print("congratulation!") 
             break
-
+        else:
+            console.print("Sorry wrong guess. The correct word is " + answer)
 
 if __name__ == '__main__':
     main()
